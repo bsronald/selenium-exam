39521ae3386a013907c13714d67b638e9fd24404
Ronald Butron <ronald.butron@jalasoft.com>
2017-04-13 12:10:04 -0400

    Account test

---
+++ src/test/java/com/jala/selenium/exam/AccountTest.java
@@ -0,0 +1,57 @@
+package com.jala.selenium.exam;
+
+import com.jala.selenium.exam.common.UICommonMethods;
+import com.jala.selenium.exam.pages.Account.Account;
+import com.jala.selenium.exam.pages.Account.ManageAccount;
+import com.jala.selenium.exam.pages.Dashboard;
+import com.jala.selenium.exam.pages.SignInForm;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author bsronald on 4/12/17.
+ */
+public class AccountTest {
+
+    private Dashboard dashboard;
+    private ManageAccount manageAccount;
+    private Account account;
+    private String accountId;
+
+    @BeforeTest
+    public void setup() {
+        final String username = "ronaldbutron@gmail.com";
+        final String password = "#1girlonlyU";
+        dashboard = SignInForm.loginAs(username, password);
+
+    }
+
+
+    @Test
+    public void testCreateAccount() {
+        // Given
+        account = dashboard.getHeader().navigateToAccounts();
+
+        // When
+        final String accountNameExpected = "Testing Account";
+        manageAccount = account.createAccount(accountNameExpected);
+        final String accountNameFound = manageAccount.getAccountName();
+        accountId = manageAccount.getAccountId();
+
+        // Then
+        Assert.assertEquals(accountNameFound, accountNameExpected, "Account Names are not equals");
+        account = manageAccount.getManagementHeader().clickOnAccount();
+        Assert.assertTrue(account.isAccountCreatedDisplayed(accountNameExpected));
+
+
+
+    }
+
+    @AfterTest
+    public void testTearDown() {
+        account.deleteAccount(accountId);
+        UICommonMethods.closeBrowser();
+    }
+}
